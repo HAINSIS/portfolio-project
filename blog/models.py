9e9739cb324c36e9534523c235a962393909db1c
HAINSIS <abdelhamid.hadili@gmail.com>
2021-10-12 22:04:36 +0100

    adding static files and finishing the project

--- blog/models.py
+++ blog/models.py
@@ -5,4 +5,11 @@ class Blog(models.Model):
     title = models.CharField(max_length=50)
     pub_date = models.DateTimeField()
     body = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
--- jobs/models.py
+++ jobs/models.py
@@ -3,6 +3,9 @@ from django.db.models.fields.files import ImageField
 
 # Create your models here.
 class Job(models.Model):
+    title = models.CharField(max_length=50, default="new job")
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
     
+    def __str__(self):
+        return self.title
